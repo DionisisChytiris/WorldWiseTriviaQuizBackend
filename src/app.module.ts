896cc2963785f5f4d62e0f35a0fcd18047c95864
Dionysios <dionysios8292@gmail.com>
2025-05-26 12:01:23 +0100

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { Message } from './messages/message.entity';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost', // or container name if using Docker
-      port: 5432,
-      username: 'postgres',
-      password: 'DiosCfu!8200',
-      database: 'testingNest',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // disable in production
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        synchronize: true,
+        autoLoadEntities: true,
+        ssl: {
+          rejectUnauthorized: false, // if needed on Render
+        },
+      }),
     }),
     MessagesModule,
   ],
