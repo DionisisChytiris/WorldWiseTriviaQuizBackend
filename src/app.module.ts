ec97ce78cc2c8df9deb9b29c561968402ff55214
Dionysios <dionysios8292@gmail.com>
2025-06-10 11:04:18 +0100

    aserd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+// import { Message } from './messages/message.entity';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        url: process.env.DATABASE_URL,
-        synchronize: true,
-        autoLoadEntities: true,
-        ssl: {
-          rejectUnauthorized: false, // if needed on Render
-        },
-      }),
+    ConfigModule.forRoot({
+      isGlobal: true, // makes .env vars available app-wide via process.env
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: {
+        rejectUnauthorized: false, // important for Neon SSL
+      },
+      synchronize: true,
+      // synchronize: process.env.NODE_ENV !== 'production',
     }),
     MessagesModule,
   ],
